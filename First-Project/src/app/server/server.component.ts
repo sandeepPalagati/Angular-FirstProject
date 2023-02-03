a9d1edac2d2f7fcd23aeee1b9ce4fb0d7bf36cc2
palagats <sandeep.palagati@moodys.com>
2023-02-02 19:19:37 -0500

    Finished till event binding

--- First-Project/src/app/server/server.component.ts
+++ First-Project/src/app/server/server.component.ts
@@ -1,11 +1,77 @@
-import { Component } from "@angular/core";
+import { Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
 
 @Component({
 
-    selector:'.app-server',
+    selector:'app-server',
     templateUrl:'./server.component.html',
     styleUrls:['./server.component.css']
 })
-export class ServerComponent{
+export class ServerComponent implements OnInit,OnChanges,DoCheck{
+    
+    @Input() childMessage:string | undefined;
+    __name: String = 'sandeep';
+    __isDisabled: boolean = false;
+    private __inputText: String = "see you !";
+    h1_color: String = 'rgb(255, 165, 0)';
+    h3_color: String = 'rgb(255, 165, 140)'
+
+
+
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log("Inside ngOnChanges ")
+    }
+
+    
+    ngOnInit(): void {
+        setTimeout(()=>{
+            this.__inputText = "Iam Back !";
+        },10000);
+        
+    }
+
+    ngDoCheck(): void {
+        console.log("Inside do check method "+this.__inputText)
+    }
+
+    set name(nameVal:String){
+        this.__name=nameVal;
+    }
+    get name(){
+        return this.__name;
+    }
+
+    set_isDisabled(){
+        this.__isDisabled = !this.__isDisabled;
+    }
+
+    get isDisabled(){
+        return this.__isDisabled;
+    }
+
+    changeColor_h1(){
+        this.h1_color=this.changeColor();
+    }
+    
+    changeColor_h3(event:Event){
+        
+        this.h3_color=this.changeColor();
+    }
+
+    private changeColor(){
+        
+        let redVal:number = Math.floor(Math.random()**256);
+        let greenVal:number = Math.floor(Math.random()*256);
+        let blueVal:number = Math.floor(Math.random()*256);
+
+        let finalVal: String = `rgb(${redVal},${greenVal},${blueVal})`;
+        return finalVal;
+    }
+
+    get inputText():String{
+        return this.__inputText;
+    }
+    onInput(event:any){
+        this.__inputText = event.target.value;
+    }
 
 }
--- First-Project/src/app/servers/servers.component.ts
+++ First-Project/src/app/servers/servers.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ServerComponent } from '../server/server.component';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent{
 
 }
